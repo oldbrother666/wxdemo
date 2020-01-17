9ddbd280b4d5bd462f944c7a807bc9c1efce1705
kangzheng <875476300@qq.com>
2020-01-17 11:08:30 +0800

    首页index样式完成

---
+++ comments/searchinput/searchinput.js
@@ -0,0 +1,25 @@
+// comments/searchinput/searchinput.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    search:'搜索'
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    onTap() {
+      this.triggerEvent('myevent', '666')
+    }
+  }
+})
